0fa0489a25ba3f76a07cc5cce4a77989c54bb7a0
Michal Lenc <michallenc@seznam.cz>
2023-04-29 11:42:56 +0200

    examples: add adjtime example
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

---
+++ examples/adjtime/adjtime_main.c
@@ -0,0 +1,53 @@
+/****************************************************************************
+ * apps/examples/hello/hello_main.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * hello_main
+ ****************************************************************************/
+
+int main(int argc, FAR char *argv[])
+{
+  printf("Hello, World!!\n");
+
+  struct timeval     delta;
+  struct timeval     olddelta;
+  int sc;
+
+  delta.tv_sec = 0;
+  delta.tv_usec = 1000000;
+  olddelta.tv_sec = 0;
+  olddelta.tv_usec = 0;
+  
+  sc = adjtime( &delta, NULL );
+
+  return 0;
+}
