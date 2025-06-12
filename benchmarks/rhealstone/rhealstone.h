c0153dd8a1a0260064554e3959294f5180101be0
Michal Lenc <michallenc@seznam.cz>
2025-06-12 15:22:54 +0200

    wip
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

---
+++ benchmarks/rhealstone/rhealstone.h
@@ -0,0 +1,112 @@
+/****************************************************************************
+ * apps/benchmarks/rhealstone/rhealstone.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+ struct performance_time_s
+{
+  clock_t start;
+  clock_t end;
+};
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: performance_thread_create
+ *
+ * Description:
+ *   Creates the thread with SCHED_FIFO policy.
+ *
+ * Input Parameters:
+ *   entry - routine to be started
+ *   arg - optional argument passed to the routine
+ *   priority - priority of the routine 
+ *
+ * Returned Value:
+ *   ID of the newly created thread.
+ *
+ ****************************************************************************/
+
+int performance_thread_create(FAR void *(*entry)(FAR void *), FAR void *arg,
+                              int priority);
+
+/****************************************************************************
+ * Name: performance_start
+ *
+ * Description:
+ *   Starts the performance measurement.
+ *
+ * Input Parameters:
+ *   result - pointer to performance_time_s structure
+ *
+ ****************************************************************************/
+
+void performance_start(FAR struct performance_time_s *result);
+
+/****************************************************************************
+ * Name: performance_end
+ *
+ * Description:
+ *   Stops the performance measurement.
+ *
+ * Input Parameters:
+ *   result - pointer to performance_time_s structure
+ *
+ ****************************************************************************/
+
+void performance_end(FAR struct performance_time_s *result);
+
+/****************************************************************************
+ * Name: performance_gettime
+ *
+ * Description:
+ *   Calculates the time between performance_start and performance_end
+ *   calls.
+ *
+ * Input Parameters:
+ *   result - pointer to performance_time_s structure
+ *
+ ****************************************************************************/
+
+size_t performance_gettime(FAR struct performance_time_s *result);
+
+/****************************************************************************
+ * Name: semaphore_shuffle
+ *
+ * Description:
+ *   Calculates the time between performance_start and performance_end
+ *   calls.
+ *
+ * Input Parameters:
+ *   count - number of times the test is performed
+ *
+ ****************************************************************************/
+
+size_t semaphore_shuffle(size_t count);
---
+++ benchmarks/rhealstone/semaphore.c
@@ -0,0 +1,165 @@
+/****************************************************************************
+ * apps/benchmarks/rhealstone/semaphore.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <assert.h>
+#include <limits.h>
+#include <pthread.h>
+#include <sched.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdatomic.h>
+#include <sys/param.h>
+#include <sys/poll.h>
+
+#include <nuttx/sched.h>
+
+#include "rhealstone.h"
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+struct semaphore_shuffle_s
+{
+  sem_t semaphore;
+  int ntests;
+  atomic_uint count;
+  bool measure_semaphores;
+
+  size_t context_switch_overhead;
+  size_t measured_time;
+};
+
+/****************************************************************************
+ * Private Functions Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+static FAR void *sem_task2(FAR void *arg) {
+  struct semaphore_shuffle_s *priv = (struct semaphore_shuffle_s *)arg;
+  struct performance_time_s result;
+  ssize_t elapsed;
+
+  performance_start(&result);
+  for (priv->count = 0; priv->count < priv->ntests; priv->count++)
+    {
+      if (priv->measure_semaphores)
+        {
+          sem_wait(&priv->semaphore);
+        }
+
+      sched_yield();
+      if (priv->measure_semaphores)
+        {
+          sem_post(&priv->semaphore);
+        }
+
+      sched_yield();
+    }
+
+  performance_end(&result);
+  elapsed = performance_gettime(&result);
+  if (!priv->measure_semaphores)
+    {
+      priv->context_switch_overhead = elapsed;
+    }
+  else
+    {
+      priv->measured_time = (elapsed - priv->context_switch_overhead) /
+                            (priv->ntests * 2);
+    }
+
+  return NULL;
+}
+
+static FAR void *sem_task1(FAR void *arg) {
+  struct semaphore_shuffle_s *priv = (struct semaphore_shuffle_s *)arg;
+  pthread_t task;
+
+  task = performance_thread_create(sem_task2, arg, CONFIG_INIT_PRIORITY + 1);
+
+  sched_yield();
+
+  while (priv->count < priv->ntests)
+    {
+      if (priv->measure_semaphores)
+        {
+          sem_wait(&priv->semaphore);
+        }
+
+      sched_yield();
+      if (priv->measure_semaphores)
+        {
+          sem_post(&priv->semaphore);
+        }
+
+      sched_yield();
+    }
+
+  pthread_join(task, NULL);
+  return NULL;
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+size_t semaphore_shuffle(size_t count)
+{
+  pthread_t task;
+
+  struct semaphore_shuffle_s priv =
+    {
+      .measure_semaphores = false,
+      .ntests = count,
+    };
+
+  sem_init(&priv.semaphore, 0, 1);
+  
+  task = performance_thread_create(sem_task1, &priv,
+                                   CONFIG_INIT_PRIORITY + 1);
+  pthread_join(task, NULL);
+
+  priv.measure_semaphores = true;
+  priv.count = 0;
+  task = performance_thread_create(sem_task1, &priv,
+                                   CONFIG_INIT_PRIORITY + 1);
+  pthread_join(task, NULL);
+  sem_destroy(&priv.semaphore);
+  return priv.measured_time;
+}
